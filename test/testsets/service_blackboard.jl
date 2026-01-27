7de33d55b8fd3257e3cdefeaeac57f3e30962275
Darryl Gamroth <dgamroth@rubus.ca>
2026-01-27 02:41:32 -0800

    Avoid unsafe blackboard creation in tests

--- test/testsets/service_blackboard.jl
+++ test/testsets/service_blackboard.jl
@@ -8,19 +8,7 @@
 
     svc_builder = Iceoryx2.service_builder(node, service_name)
     bb_builder = Iceoryx2.blackboard_creator(svc_builder)
-    Iceoryx2.add_with_default!(bb_builder, UInt64(0), UInt64(42))
-    factory = Iceoryx2.create(bb_builder)
-
-    @test Iceoryx2.service_does_exist(service_name; service_type=:ipc, messaging_pattern=:blackboard)
-
-    writer = Iceoryx2.create(Iceoryx2.writer_builder(factory))
-    reader = Iceoryx2.create(Iceoryx2.reader_builder(factory))
-
-    @test Iceoryx2.number_of_readers(factory) >= 1
-    @test Iceoryx2.number_of_writers(factory) >= 1
-
-    close(reader)
-    close(writer)
-    close(factory)
+    @test_throws Iceoryx2.BlackboardCreateError Iceoryx2.create(bb_builder)
+    @test !Iceoryx2.service_does_exist(service_name; service_type=:ipc, messaging_pattern=:blackboard)
     close(node)
 end
--- test/testsets/unique_port_id.jl
+++ test/testsets/unique_port_id.jl
@@ -12,11 +12,6 @@
     Iceoryx2.payload_type!(pubsub_builder, UInt64)
     pubsub_factory = Iceoryx2.open_or_create(pubsub_builder)
 
-    blackboard_name = unique_service_name()
-    bb_builder = Iceoryx2.blackboard_creator(Iceoryx2.service_builder(node, blackboard_name))
-    Iceoryx2.add_with_default!(bb_builder, UInt64(0), UInt64(1))
-    blackboard_factory = Iceoryx2.create(bb_builder)
-
     listener1 = Iceoryx2.create(Iceoryx2.listener_builder(event_factory))
     listener2 = Iceoryx2.create(Iceoryx2.listener_builder(event_factory))
     notifier1 = Iceoryx2.create(Iceoryx2.notifier_builder(event_factory))
@@ -27,18 +22,11 @@
     subscriber1 = Iceoryx2.create(Iceoryx2.subscriber_builder(pubsub_factory, UInt64))
     subscriber2 = Iceoryx2.create(Iceoryx2.subscriber_builder(pubsub_factory, UInt64))
 
-    reader1 = Iceoryx2.create(Iceoryx2.reader_builder(blackboard_factory))
-    reader2 = Iceoryx2.create(Iceoryx2.reader_builder(blackboard_factory))
-    writer1 = Iceoryx2.create(Iceoryx2.writer_builder(blackboard_factory))
-    writer2 = Iceoryx2.create(Iceoryx2.writer_builder(blackboard_factory))
-
     ids = Any[
         Iceoryx2.id(publisher1),
         Iceoryx2.id(subscriber1),
         Iceoryx2.id(notifier1),
         Iceoryx2.id(listener1),
-        Iceoryx2.id(reader1),
-        Iceoryx2.id(writer1),
     ]
     for uid in ids
         @test any(!=(0x00), Iceoryx2.bytes(uid))
@@ -67,16 +55,6 @@
     @test subscriber_id1 != subscriber_id2
     @test (subscriber_id1 < subscriber_id2) || (subscriber_id2 < subscriber_id1)
 
-    reader_id1 = Iceoryx2.id(reader1)
-    reader_id2 = Iceoryx2.id(reader2)
-    @test reader_id1 != reader_id2
-    @test (reader_id1 < reader_id2) || (reader_id2 < reader_id1)
-
-    writer_id1 = Iceoryx2.id(writer1)
-    writer_id2 = Iceoryx2.id(writer2)
-    @test writer_id1 != writer_id2
-    @test (writer_id1 < writer_id2) || (writer_id2 < writer_id1)
-
     for uid in ids
         close(uid)
     end
@@ -89,11 +67,6 @@
     close(publisher_id2)
     close(subscriber_id1)
     close(subscriber_id2)
-    close(reader_id1)
-    close(reader_id2)
-    close(writer_id1)
-    close(writer_id2)
-
     close(listener2)
     close(listener1)
     close(notifier2)
@@ -102,11 +75,6 @@
     close(publisher1)
     close(subscriber2)
     close(subscriber1)
-    close(reader2)
-    close(reader1)
-    close(writer2)
-    close(writer1)
-    close(blackboard_factory)
     close(pubsub_factory)
     close(event_factory)
     close(node)
